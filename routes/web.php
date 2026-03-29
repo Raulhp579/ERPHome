462f7dffcea6addb1ece4bec8898b5fa4af11505
Raulhp579 <raulhenares5@gmail.com>
2026-03-29 15:01:28 +0200

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
 
-Route::get('/{any}', function () {
-    return file_get_contents(public_path('angular/index.html'));
+Route::get('/{any}', function ($any = '') {
+    $angularDist = public_path('angular/browser');
+
+    // Serve static Angular assets directly
+    $filePath = $angularDist . '/' . $any;
+    if ($any !== '' && file_exists($filePath) && !is_dir($filePath)) {
+        $mime = mime_content_type($filePath);
+        // Fix MIME type for JS modules
+        if (str_ends_with($any, '.js')) {
+            $mime = 'application/javascript';
+        } elseif (str_ends_with($any, '.css')) {
+            $mime = 'text/css';
+        }
+        return Response::make(file_get_contents($filePath), 200, ['Content-Type' => $mime]);
+    }
+
+    // SPA fallback
+    return Response::make(
+        file_get_contents($angularDist . '/index.html'),
+        200,
+        ['Content-Type' => 'text/html']
+    );
 })->where('any', '.*');
